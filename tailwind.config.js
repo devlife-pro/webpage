a6326b397f06c6e36dda4c50c0d52cd69448a807
Kawium <43068452+Kawium@users.noreply.github.com>
2023-01-25 13:25:44 +0100

    added daisyui and footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [
-    require('@tailwindcss/line-clamp')
-  ],
-}
+  plugins: [require("@tailwindcss/line-clamp"), require("daisyui")],
+  daisyui: {
+    styled: true,
+    themes: true,
+    base: false,
+    utils: true,
+    logs: false,
+    rtl: false,
+    prefix: "",
+    darkTheme: "dark",
+  },
+};
