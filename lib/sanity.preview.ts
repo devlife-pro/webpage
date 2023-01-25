8183b40632650cebf2e374b5ade79ed14e271304
rchtect <rchtectdev@gmail.com>
2023-01-25 12:07:14 +0100

    trying to fix vercel preview

--- lib/sanity.preview.ts
+++ lib/sanity.preview.ts
@@ -1,17 +1,18 @@
-"use client";
-
 import { definePreview } from "next-sanity/preview";
 import { projectId, dataset } from "./sanity.client";
 
+
 if (!projectId || !dataset) {
   throw new Error("Missing project id or dataset. Check sanity.json or .env");
+}
+
+function onPublicAccessOnly() {
+  throw new Error(`Unable to load preview as you're not logged in`)
 }
 
 
 export const usePreview = definePreview({
     projectId,
     dataset,
-    // onPublicAccessOnly: () => {
-    //   throw new Error('Not authenticated - preview not available')
-    // }
+    onPublicAccessOnly,
 })
