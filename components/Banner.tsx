adf23c4679021588d102120e980a014add48ffad
rchtect <rchtectdev@gmail.com>
2023-01-26 14:48:23 +0100

    minor ui tweaks

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -2,7 +2,7 @@ function Banner() {
   return (
     <div className=" mt-12">
       <h3 className="text-[#ff0083] items-center flex justify-center">
-        DEVLIFE - LIMITED BETA RELEASE
+        LIMITED BETA RELEASE
       </h3>
       <div className="flex flex-col lg:space-x-5 justify-between font-bold px-10 py-5 mb-10 items-center">
         <div>
