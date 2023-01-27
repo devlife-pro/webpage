e29c9bf416f35a5f6868a21ced36bb842b7bcc3d
rchtect <rchtectdev@gmail.com>
2023-01-27 12:08:00 +0100

    added uturn icon

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,10 @@
+import { ArrowUturnLeftIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
 import Link from "next/link";
 
 function Header() {
   return (
-    <header className="flex items-center justify-between space-x-2 font-bold px-10 py-5 bg-[#151016] backdrop-blur-lg sticky top-0 dark:bg-[#151016]/60 bg-white/60 rounded-md z-10 h-22 lg:h-16 md:h-18 mt-4 4k:h-24 border-gray-300 border-solid border-[0.1px] max-w-4xl mx-auto 4k:max-w-[2000px]">
+    <header className="flex items-center justify-between space-x-2 font-bold px-10 py-5 bg-[#151016] backdrop-blur-lg sticky top-0 dark:bg-[#151016]/60 bg-white/60 rounded-md z-10 h-22 lg:h-16 md:h-18 mt-4 4k:h-24 border-gray-900 border-solid border-[0.1px] max-w-4xl mx-auto 4k:max-w-[2000px]">
       <div className="flex items-center space-x-2">
         <Link href="/">
           <Image
@@ -15,9 +16,11 @@ function Header() {
           />
         </Link>
         <Link href="/">
-          <h1 className="text-white md:text-base text-2xl 4k:text-3xl">
+          <h1 className="text-white md:text-base text-2xl 4k:text-3xl flex justify-center items-center">
             Devlife
+            <ArrowUturnLeftIcon className="ml-2 h-4 w-4" />
           </h1>
+
         </Link>
       </div>
       <div></div>
