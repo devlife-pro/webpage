20ca67c87acd3f752bbd026d8f56f4d0f29283ac
Kawium <43068452+Kawium@users.noreply.github.com>
2023-01-27 13:41:42 +0100

    fixes

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 function Header() {
   return (
-    <header className="flex items-center justify-between space-x-2 font-bold px-10 py-5 bg-[#151016] backdrop-blur-lg sticky top-0 dark:bg-[#151016]/60 bg-white/60 rounded-md z-10 h-22 lg:h-16 md:h-18 mt-4 4k:h-24 border-gray-900 border-solid border-[0.1px] max-w-4xl mx-auto 4k:max-w-[2000px]">
+    <header className="flex items-center justify-between space-x-2 font-bold px-10 py-5 bg-[#151016] backdrop-blur-[3.5px] sticky top-0 dark:bg-[#151016]/60 bg-white/60 rounded-md z-10 h-22 lg:h-16 md:h-18 mt-4 4k:h-24 border-gray-300 border-solid border-[0.1px] max-w-4xl mx-auto 4k:max-w-[2000px]">
       <div className="flex items-center space-x-2">
         <Link href="/">
           <Image
@@ -20,7 +20,6 @@ function Header() {
             Devlife
             <ArrowUturnLeftIcon className="ml-2 h-4 w-4" />
           </h1>
-
         </Link>
       </div>
       <div></div>
