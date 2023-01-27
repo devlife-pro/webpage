a1f7b29e8b88b5f4db84bd03334d0f07ee1c085e
Kawium <43068452+Kawium@users.noreply.github.com>
2023-01-27 13:23:45 +0100

    fix

--- components/BlogList.tsx
+++ components/BlogList.tsx
@@ -10,7 +10,6 @@ type Props = {
 function BlogList({ posts }: Props) {
   return (
     <div className="max-w-6xl mx-auto 4k:max-w-[2000px]">
-
       <div className="grid grid-cols-1 lg:grid-cols-3 px-10 gap-10 gap-y-16 pb-24 4k:grid-cols-4">
         {posts.map((post) => (
           <ClientSideRoute key={post._id} route={`/post/${post.slug.current}`}>
@@ -36,7 +35,7 @@ function BlogList({ posts }: Props) {
 
                   <div className="flex flex-col md:flex-row gap-y-2 md:gap-x-2 items-center">
                     {post.categories.map((category) => (
-                      <div className="bg-purple-400 text-center text-black py-1 px-3 rounded text-sm font-semibold" >
+                      <div className="bg-purple-400 text-center text-black py-1 px-3 rounded text-sm font-semibold">
                         <p>#{category.title}</p>
                       </div>
                     ))}
@@ -49,25 +48,25 @@ function BlogList({ posts }: Props) {
                 <p className="line-clamp-2 text-white">{post.description}</p>
               </div>
               <div className="flex justify-between items-center mt-4 text-center">
-              <p className="mt-5 font-bold flex items-center group-hover:underline">
-                Read Post
-                <ArrowUpRightIcon className="ml-2 h-4 w-4" />
-              </p>
-              <div className="flex items-center space-x-2 justify-center">
-                <Image
-                  className="rounded-full object-cover h-10 w-10 4k:h-14 4k:w-14"
-                  src={urlFor(post.author.image).url()}
-                  alt={post.author.name}
-                  width={100}
-                  height={100}
-                />
-                <div className="">
-                  <h3 className="text-lg font-bold 4k:text-2xl group-hover:underline bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
-                    {post.author.name}
-                  </h3>
-                  {/* TODO: Author bio */}
+                <p className="mt-5 font-bold flex items-center group-hover:underline">
+                  Read Post
+                  <ArrowUpRightIcon className="ml-2 h-4 w-4" />
+                </p>
+                <div className="flex items-center space-x-2 justify-center">
+                  <Image
+                    className="rounded-full object-cover h-10 w-10 4k:h-14 4k:w-14"
+                    src={urlFor(post.author.image).url()}
+                    alt={post.author.name}
+                    width={100}
+                    height={100}
+                  />
+                  <div className="">
+                    <h3 className="text-lg font-bold 4k:text-2xl group-hover:underline bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+                      {post.author.name}
+                    </h3>
+                    {/* TODO: Author bio */}
+                  </div>
                 </div>
-              </div>
               </div>
             </div>
           </ClientSideRoute>
